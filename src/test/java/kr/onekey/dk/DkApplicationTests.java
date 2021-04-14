afd0398438153fc9d8312c37df992ddc573d41f9
kangyoonwon <yoonwon.kang@onekey.kr>
2021-04-14 16:32:37 +0900

    기본 gradle 및 spring boot 설정 / firebase 테스트 작업 추가중

---
+++ src/test/java/kr/onekey/dk/DkApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.onekey.dk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
